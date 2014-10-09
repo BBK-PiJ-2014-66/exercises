d89b7be8da891343e3ec944ac44405347afbb0ac
Oliver Smart <osmart@smartsci.uk>
2014-10-09 02:23:47 +0100

    Reformat code with eclipse to get used to standard java syntax

--- day01/E06Division.java
+++ day01/E06Division.java
@@ -12,38 +12,42 @@
 
 public class E06Division {
 	public static void main(String[] args) {
-                // lets be good no methods do all in main
-                System.out.print("Please enter the 1st integer number: ");
-                int numa =  Integer.parseInt(System.console().readLine());
-                System.out.print("Please enter the 2nd integer number: ");
-                int numb =  Integer.parseInt(System.console().readLine());
+		// lets be good no methods do all in main
+		System.out.print("Please enter the 1st integer number: ");
+		int numa = Integer.parseInt(System.console().readLine());
+		System.out.print("Please enter the 2nd integer number: ");
+		int numb = Integer.parseInt(System.console().readLine());
 
 		// protect against divide by zero
-		if (numb==0) {
-			System.out.println(numa + " divided by " + numb + " is infinity"); 
-		}
-		else {
-                	boolean lSignsDiffer=false; // set true if numa and numb have difference signs
-                	if (numa < 0) lSignsDiffer = (!lSignsDiffer);
-                	if (numb < 0) lSignsDiffer = (!lSignsDiffer);
-             
-                	// method is to start the "remainder" at numa and subtract numb repeatedly 
-                	// until the "remainder" is less than numb.
-                	int remainder = (numa<0) ? -numa : numa; // absolute value of numa
-                	int absnumb = (numb < 0) ? -numb : numb; // absolute value of numb
-                	int quotient=0;
-                	while( remainder > absnumb) {
+		if (numb == 0) {
+			System.out.println(numa + " divided by " + numb + " is infinity");
+		} else {
+			boolean lSignsDiffer = false; // set true if numa and numb have
+											// difference signs
+			if (numa < 0)
+				lSignsDiffer = (!lSignsDiffer);
+			if (numb < 0)
+				lSignsDiffer = (!lSignsDiffer);
+
+			// method is to start the "remainder" at numa and subtract numb
+			// repeatedly
+			// until the "remainder" is less than numb.
+			int remainder = (numa < 0) ? -numa : numa; // absolute value of numa
+			int absnumb = (numb < 0) ? -numb : numb; // absolute value of numb
+			int quotient = 0;
+			while (remainder > absnumb) {
 				remainder -= absnumb;
 				quotient++;
 			}
-                	if (lSignsDiffer) 
+			if (lSignsDiffer)
 				quotient = -quotient;
-			if (numa<0)
-                        	remainder = -remainder;
-			System.out.println(numa + " divided by " + numb + " is " + 
-                                   	quotient + ", remainder " + remainder);
+			if (numa < 0)
+				remainder = -remainder;
+			System.out.println(numa + " divided by " + numb + " is " + quotient
+					+ ", remainder " + remainder);
 		}
 
-		//System.out.println("debug to just to check numa/numb= " + numa/numb + " numa%numb= " + numa%numb);
+		// System.out.println("debug to just to check numa/numb= " + numa/numb +
+		// " numa%numb= " + numa%numb);
 	}
 }
