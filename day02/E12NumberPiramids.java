38bf793fe802225c7d3c39c42ab3e874ea9e7342
Oliver Smart <osmart@smartsci.uk>
2014-10-09 18:24:47 +0100

    initial version with spec

---
+++ day02/E12NumberPiramids.java
@@ -0,0 +1,29 @@
+/* 
+ * Birbeck MSc Computer Science PiJ coursework From September 2014
+ *  
+ * Day 2 Exercise 12 Number piramids
+ * task set:
+ * Write a program that output a number piramid like the one below.
+ * 1
+ * 22
+ * 333
+ * 4444
+ * 55555
+ * 666666
+ * 7777777
+ * \ldots
+ * 
+ * Not allowed methods and everything must be in main.
+ * assune the ldots is a typo from latex.
+ * Not told the user is to have any control. 
+ * First code to do seven as shown.
+ * Then extend to allow user to control number using a command line argument 
+ * 
+ *  @author Oliver S. Smart
+ */
+
+public class E12NumberPiramids {
+	public static void main(String[] args) {
+
+	}
+}
