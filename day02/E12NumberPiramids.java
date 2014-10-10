5667fb6f97018d83970c6ede105bc108b9c5b2f7
Oliver Smart <osmart@smartsci.uk>
2014-10-10 06:12:59 +0100

    parse command line but do not yet protect against non-integer input

--- day02/E12NumberPiramids.java
+++ day02/E12NumberPiramids.java
@@ -25,10 +25,27 @@
 public class E12NumberPiramids {
 	public static void main(String[] args) {
 		int numbLines =  7; // default value will allow override
-		for (int lc=0; lc < numbLines; lc++) { // line count
-			for (int cc=0; cc < lc+1; cc++) // column count for lc=0 want 1 output of 1, lc=1 want two output of 2 ..
-				System.out.print(lc+1); 
-			System.out.println(); // final carriage return to end the line
+                
+ 		// parse command lines to get numbLines
+		if (args.length==0) { // no command line args
+			 System.out.println("Using default number of lines = " + numbLines);
+			 System.out.println("To produce a number piramid with NLINES rerun");
+			 System.out.println();
+			 System.out.println("    java E12NumberPiramids NLINES");
+			 System.out.println();
+			 System.out.println("where NLINES is a postive integer");
+		}
+		else if (args.length == 1) {
+			numbLines = Integer.parseInt(args[0]);
+		}
+
+                // now produce the piramid
+		for (int lc=0; lc < numbLines; lc++) { // line count from zero as per C not FORTRAN
+                        int numbOut = lc + 1; // output 1, 2, 3
+			for (int cc=0; cc < numbOut; cc++) {// column count for lc=0 want 1 output of 1, lc=1 want two output of 2 ..
+				System.out.print(numbOut%10); // for 10 output 0, 11 output 1 to keep piramid straight
+                        }
+			System.out.println(); // final carriage return to end each line
 		}
 	}
 }
