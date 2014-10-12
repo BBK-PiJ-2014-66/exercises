048ba3d5beab66590a3f7503cea460607f7ad3b0
Oliver Smart <osmart@smartsci.uk>
2014-10-12 16:11:56 +0100

    Initial commit just document task & initial ideas

---
+++ day03/E14InsideOrOutside.java
@@ -0,0 +1,17 @@
+/* 
+ * Birbeck MSc Computer Science PiJ coursework From September 2014
+ *  
+ * Day 3 Exercise 14 Inside or outside
+ * task set:
+ *    Write a program that reads the coordinates of the two points 
+ *    defining a rectangle and then the coordinates of a third 
+ *    point. The program must then determine whether the point 
+ *    falls inside or outside the rectangle.
+ *
+ * Initial ideas:
+ *    A simple extension of Exercise 13. 
+ *    May as well add explicit constructors for the Point class
+ *    to end up with more elegant code.
+ *
+ *  @author Oliver S. Smart
+ */
