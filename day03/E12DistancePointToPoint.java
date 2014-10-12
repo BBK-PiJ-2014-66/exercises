5c6f05fb27796fb857c287282f242ebb6e5ff68d
Oliver Smart <osmart@smartsci.uk>
2014-10-12 10:07:39 +0100

    revise introducing method outputClosestOf3Points that deals with case of points on isosceles triangle where two distances can be smallest or equilateral where all three distances are equal. The use of a function allows -test option to use three test cases demonstrating that the method works. Note the use of Math.abs() to compare doubles.

--- day03/E12DistancePointToPoint.java
+++ day03/E12DistancePointToPoint.java
@@ -34,6 +34,28 @@ class Point {
 }
 
 public class E12DistancePointToPoint {
+	static void outputClosestOf3Points( Point pntA, Point pntB, Point pntC) { 
+		// first echo back information to user - have the understood input order
+		System.out.print("pntA stored as " + pntA.coordString() );
+		System.out.print("   pntB stored as " + pntB.coordString() );
+		System.out.println("   pntC stored as " + pntC.coordString() );
+		double distAB = pntA.distanceTo(pntB);
+		double distAC = pntA.distanceTo(pntC);
+		double distBC = pntB.distanceTo(pntC);
+		System.out.println("Calculate distances: AB=" + distAB + " AC= " + distAC + "  BC= " + distBC);
+ 
+		System.out.print("Closest pair(s) of points are ");
+		// must allow for two or three of the distances to be the same
+		double minDist = Math.min(distAB,distAC);
+	        minDist = Math.min(minDist,distBC);	
+		if (Math.abs(distAB-minDist) < 1E-6) 
+			System.out.print("AB  ");
+		if (Math.abs(distAC-minDist) < 1E-6) 
+			System.out.print("AC  ");
+		if (Math.abs(distBC-minDist) < 1E-6) 
+			System.out.print("BC  ");
+		System.out.println();
+	}
 	public static void main(String[] args) {
 		System.out.println("E12DistancePointToPoint");
 		System.out.println("program reads in X and Y coordinates for three points");
@@ -56,17 +78,37 @@ public class E12DistancePointToPoint {
 			// leave A at (0,0) default constructor
 			pointB.storexy(3.,4.); // B is (3,4) 
 			pointC.storexy(1.,1.); // C is (1,1) 
-			System.out.println("\tstored A as (0,0) B as (3,4) and C as (1,1)");
-			System.out.println("\tso by hand distAB=5"); 
-			System.out.println("\t           distAC=sqrt(2)=1.41421");
-			System.out.println("\t           distBC=sqrt(13)=3.60555");
-			System.out.println("\tSo closest points should be A and C");
+			System.out.println();
+			System.out.println("\ttest#1 store A as (0,0) B as (3,4) and C as (1,1)");
+			System.out.println("\ttest#1 so by hand distAB=5  distAC=sqrt(2)=1.41421 distBC=sqrt(13)=3.60555");
+			System.out.println("\ttest#1 So closest pair of points should be AC");
+			System.out.println("\ttest#1 function outputClosestOf3Points reports:");
+	        	outputClosestOf3Points( pointA, pointB, pointC); 
+
+			pointA.storexy(0.,1.); 
+			pointB.storexy(2.,1.); 
+			pointC.storexy(1.,0.); 
+			System.out.println();
+			System.out.println("\ttest#2 store A as (0,1) B as (2,1) and C as (1,0)");
+			System.out.println("\ttest#2 distAC = distBC so should output two pairs AC and BC");
+			System.out.println("\ttest#2 function outputClosestOf3Points reports:");
+	        	outputClosestOf3Points( pointA, pointB, pointC); 
+
+			pointA.storexy(0.,0.); 
+			pointB.storexy(1.,0.); 
+			pointC.storexy(0.5,Math.sqrt(0.75)); 
+			System.out.println();
+			System.out.println("\ttest#3 store A as (0,0) B as (1,0) and C as (0.5,sqrt{0.75})");
+			System.out.println("\ttest#3 equilateral triangle should output all three pairs");
+			System.out.println("\ttest#3 function outputClosestOf3Points reports:");
+	        	outputClosestOf3Points( pointA, pointB, pointC); 
+
                 }
 		else if (args.length!=6) {
 			System.out.print(usageMessage);
 			System.exit(0); // terminate program with no error 
 		}
-		else {
+		else {  // user supplied args
 			double d0 = Double.parseDouble(args[0]);
 			double d1 = Double.parseDouble(args[1]);
 			double d2 = Double.parseDouble(args[2]);
@@ -76,26 +118,10 @@ public class E12DistancePointToPoint {
 			pointA.storexy(d0,d1);
 			pointB.storexy(d2,d3);
 			pointC.storexy(d4,d5);
+	        	outputClosestOf3Points( pointA, pointB, pointC); 
 		}
 		
-		// first echo back information to user - have the understood input order
-		System.out.print("pointA stored as " + pointA.coordString() );
-		System.out.print("   pointB stored as " + pointB.coordString() );
-		System.out.println("   pointC stored as " + pointC.coordString() );
-		double distAB = pointA.distanceTo(pointB);
-		double distAC = pointA.distanceTo(pointC);
-		double distBC = pointB.distanceTo(pointC);
-		System.out.println("Calculate distances: AB=" + distAB + " AC= " + distAC + "  BC= " + distBC);
- 
-		System.out.print("Closest points are ");
-		if ( (distAB<distAC) && (distAB < distBC) ) {
-			System.out.println("A and B");
-		}
-		else if ( (distAC<distAB) && (distAC<distBC) ) {
-			System.out.println("A and C");
-		}
-		else {
-			System.out.println("B and C");
-		}
+                
+
 	}
 }
