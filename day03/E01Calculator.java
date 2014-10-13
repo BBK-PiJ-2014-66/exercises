da11bc22ebcb6f131aef5901ffa6ad9786383496
Oliver Smart <osmart@smartsci.uk>
2014-10-13 16:16:13 +0100

    Start with comment quoting task & my initial ideas

---
+++ day03/E01Calculator.java
@@ -0,0 +1,23 @@
+/* 
+ * Birbeck MSc Computer Science PiJ coursework From September 2014
+ *  
+ * Day 3 Exercise 1 Calculator
+ * task set:
+ *    Write a program that reads two numbers from the user and then offers a 
+ *    menu with the four basic operations: addition, subtraction, 
+ *    multiplication, and division. Once the user has selected an operation
+ *    from the menu, the calculator performs the operation.
+ * 
+ *    Hint: In the same way that there exists an Integer.parseInt() method 
+ *    to parse integers, there is a Double.parseDouble() method to parse 
+ *    real numbers.
+ *
+ *  @author Oliver S. Smart
+ *
+ *  Initial thoughts
+ * (a) has to be interactive
+ * (b) task does not decribe what to after the calculator performs
+ *     the operation. But as it is a menu lets allow another operation.
+ * (c) User can terminate program by hitting enter at any prompt
+ */
+
