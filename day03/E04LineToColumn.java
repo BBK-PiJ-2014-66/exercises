a042ada962c86c2745f29f629f1b64742f429c7e
Oliver Smart <osmart@smartsci.uk>
2014-10-14 20:02:17 +0100

    Does what it says on the tin

--- day03/E04LineToColumn.java
+++ day03/E04LineToColumn.java
@@ -19,10 +19,11 @@
  *
  * 	Use for loop with String str.length() and String 
  *	str.Atchar() to go thru' string character by character
- *      call it imyChar
+ *      call it myChar
  *
  *	First task simple output the myChar on a line of its own
- *	using println
+ *	using println. Assume in this case "letter" includes spaces
+ *      and punctuation?
  *
  *	Second task:
  *      print myChar that are not "spaces" without a newline. 
@@ -32,6 +33,41 @@
  *	Must not forget to make sure last word output is terminated by
  *	a (single) newline
  *	
- *	A space is " " or \t
+ *	A space is " " or \t or \n anything else that 
+ *	Character.isWhitespace method picks up
  *
+ * test with  
+ * The 123  quick brown fox, jumps	over the lazy dog.
  */
+public class E04LineToColumn {
+	public static void main(String[] args) {
+        	System.out.print("Please enter text to process: ");
+        	String inStr = System.console().readLine();
+		System.out.println("Your text with each character on a single line:");
+		for (int cc=0; cc<inStr.length() ; cc++) {
+			char inStrChar = inStr.charAt(cc);
+			System.out.println(inStrChar);
+		}
+		System.out.println("Your text with each \"letter\" on a single line (digits, spaces and punctuation ignored):");
+		for (int cc=0; cc<inStr.length() ; cc++) {
+			char inStrChar = inStr.charAt(cc); 
+			if (Character.isLetter(inStrChar))
+				System.out.println(inStrChar);
+		}
+		System.out.println("Your text with each word on a separate line (a \"word\" is anything surrounded by whitespace): ");
+		boolean previousSpace = true; // to avoid outputting initial newline if the text starts with a space
+		for (int cc=0; cc<inStr.length() ; cc++) {
+			char inStrChar = inStr.charAt(cc); 
+			if (Character.isWhitespace(inStrChar)) {
+				if (!previousSpace) System.out.println(); // newline to complete word
+			 	previousSpace = true; 
+  			}
+			else {
+				System.out.print(inStrChar);
+			 	previousSpace = false;
+			}
+		}
+		if (!previousSpace) System.out.println(); // newline to complete the last word
+	
+	}
+}
