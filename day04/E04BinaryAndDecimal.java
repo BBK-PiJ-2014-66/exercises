332aa989a4662bb8f2de712c141fd67c7c0cd91d
Oliver Smart <osmart@smartsci.uk>
2014-10-20 19:04:59 +0100

    Comment with task and initial ideas.

---
+++ day04/E04BinaryAndDecimal.java
@@ -0,0 +1,32 @@
+/* 
+ * Birbeck MSc Computer Science PiJ coursework From September 2014
+ *  
+ * Day 4 Exercise 4 Binary and decimal
+ *
+ * task set:
+ *	Create a program in which you define the following methods:
+ *
+ *	binary2decimal(String): takes from the user a binary number (with digits 0 and
+ *	1) and returns the corresponding number in decimal (base-10, with digits
+ *	between 0 and 9). 
+ *
+ *	decimal2binary(int): the opposite of the previous one: takes a decimal number
+ *	and returns the corresponding binary number. 
+ *
+ *	The program must offer a menu to the user with two options. The first one takes 
+ *	binary number from the user and
+ *	returns the corresponding decimal number. The second one does the opposite:
+ *	takes a decimal number and returns a binary number. The program should use the
+ *	methods defined.
+ *	
+ *
+ *  @author Oliver S. Smart
+ *
+ * Initial thoughts
+ * 	What kind of error checking? Lets do a method isBinary( string) and return false
+ *	if it has something other than 0 and 1.
+ *
+ *	User menu required but first write a -test 
+ */
+
+
