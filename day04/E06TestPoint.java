ebfe5f0adbbe18d375f6ad9b753715330b6a1136
Oliver Smart <osmart@smartsci.uk>
2014-10-24 19:13:51 +0100

    testing procedure for exercise 06

---
+++ day04/E06TestPoint.java
@@ -0,0 +1,18 @@
+/* 
+ * Birbeck MSc Computer Science PiJ coursework From September 2014
+ *  
+ * Day 4 Exercise 6 More on points
+ *	This is a separate testing program for Point.java (to try out separation)
+ *
+ *  @author Oliver S. Smart
+ *
+ */ 
+class E06TestPoint{
+	public static void main(String[] args) {
+                System.out.println("E06TestPoint run some tests on Point class");
+		Point myPoint = new Point(3.,4.);
+                //System.out.println("Have defined myPoint= " + myPoint.Str);
+                System.out.println("myPoint.distanceToOrigin()=" + myPoint.distanceToOrigin() + "  (expect 5.0)");
+		
+	}
+}
