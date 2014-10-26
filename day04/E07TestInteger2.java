40da051acedae858993c813808cd2b7ceae38e13
Oliver Smart <osmart@smartsci.uk>
2014-10-26 17:11:08 +0000

    Exercise 7 Integer initial commit just task

---
+++ day04/E07TestInteger2.java
@@ -0,0 +1,36 @@
+/* 
+ * Birbeck MSc Computer Science PiJ Exercsies 
+ * author Oliver S. Smart
+ * date   from 26 Sept 2014
+ *  
+ * Day 4 Exercise 7 Integer
+ *
+ * task set:
+ * 	Create your own version of boxed int! Create a class Integer2 
+ *	(see Integer2.java)
+ * 
+ * test code given:
+	Integer2 i2 = new Integer2();
+	print "Enter a number: ";
+	String str = System.console().readLine();
+	int i = Integer.parseInt(str);
+	i2.setValue(i);
+	print "The number you entered is "
+	if (i2.isEven()) {
+		println "even.";
+	} else if (i2.isOdd()) {
+		println "odd.";
+	} else {
+		println "undefined!! Your code is buggy!";
+	}
+	int parsedInt = Integer.parseInt(i2.toString());
+	if (parsedInt == i2.getValue()) {
+		println("Your toString() method seems to work fine.");
+	}
+ *
+ * Initial thoughts
+ *
+ *	test code given is interactive (not my favourite).
+ *	to translate javadecaf to java should be easy.
+ *	prettyPrint method not tested
+ */
---
+++ day04/Integer2.java
@@ -0,0 +1,23 @@
+/* 
+ * Birbeck MSc Computer Science PiJ Exercsies 
+ * author Oliver S. Smart
+ * date   from 26 Sept 2014
+ *  
+ * Day 4 Exercise 7 Integer
+ *
+ * task set:
+ * 	Create your own version of boxed int! Create a class Integer2 
+ *	with only one field (int value) and the following methods:
+ *	getValue(): returns the value of this number as an int, a getter.
+ * 	setValue(int): a setter.
+ * 	isEven(): returns true if the number is even, false otherwise.
+ * 	isOdd(): the opposite.
+ * 	prettyPrint(): prints the value of the integer on the screen.
+ * 	toString(): returns a String equivalent to the number.
+ *
+ * then given test code 
+ *
+ * Initial thoughts
+ *	test code given is interactive and requires all methods
+ *
+ */
