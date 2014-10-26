9ebc0e98b4c989208670760a82190e28b7bedb71
Oliver Smart <osmart@smartsci.uk>
2014-10-26 19:59:03 +0000

    fix method by moving up the conditional. Not sure what the method is for!

--- day05/E01doggyMethod.java
+++ day05/E01doggyMethod.java
@@ -1,12 +1,13 @@
 class E01doggyMethod{
 	public static String doggyMethod(int n) {
-		String result = doggyMethod(n-3) + n + doggyMethod(n-2);
 		if (n <= 0) {
 			return "";
 		}
+		String result = doggyMethod(n-3) + n + doggyMethod(n-2);
 		return result;
 	}
 	public static void main(String[] args) {
-		System.out.println("doggyMethod(0)= " + doggyMethod(0));
+		for (int i=0; i<11; i++)
+			System.out.println("doggyMethod(" + i + ")= " + doggyMethod(i));
 	}
 }
