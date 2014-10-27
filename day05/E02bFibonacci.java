4505e124f9d92c07a67cd80aec7eb11fee951dd0
Oliver Smart <osmart@smartsci.uk>
2014-10-27 14:30:59 +0000

    The task and some initial thoughts

---
+++ day05/E02bFibonacci.java
@@ -0,0 +1,26 @@
+/* 
+ * Birbeck MSc Computer Science PiJ Exercsies 
+ * author Oliver S. Smart
+ * date   from 27 Sept 2014
+ *  
+ * Day 5 Exercise 2b Fibonacci
+ *
+ * task set:
+ * 
+ * 	Write a small program with a method that calculates the nth element of the
+ *	Fibonacci sequence as seen in the notes.  Is it easy to do this both
+ *	iteratively and recursively? Try both ways and see which is more natural for
+ *	you. If one takes too long, try the other way.
+ * 
+ *	When doing it recursively, do it with and without memoization. Compare the time
+ *	that is needed in each case to find F(40) or F(45).
+ * 
+ *
+ * Initial thoughts
+ *	memoization means storing results calculated in an array. 
+ *		see http://xlinux.nist.gov/dads//HTML/memoize.html
+ *	asked for "time" can get this from command line but count 
+ *	the number of function calls (use a static class variable).
+ * 
+ *
+ */
