796a4982c3c01aa32b405f32b489cadba40e4730
Oliver Smart <osmart@smartsci.uk>
2014-10-27 22:13:10 +0000

    wrote iterative method and tested it. Not challenging!

--- day05/E05Power.java
+++ day05/E05Power.java
@@ -9,9 +9,35 @@
  * 
  *	Create a class with a static method pow that takes to integers
  *	(base and exponent) and calculates the power, e.g. pow(2,3)
- *	calculates 2**3 .  Is it easy to do this both iteratively and
+ *	calculates 2**3=-8.  Is it easy to do this both iteratively and
  *	recursively?
  *
  * Initial thoughts
+ *	Lets use double for the method and handle negative exponents
  *
  */
+class E05Power{
+	public static double powIterative( int base, int exponent) {
+		double result = 1.0;
+		boolean negativeExponent = false;
+		if (exponent<0) {
+			exponent *= -1.; //change sign
+			negativeExponent = true;
+		}
+		for (int ec=1; ec<=exponent; ec++) { // count 1, 2, 3
+			result *= base;
+		}
+		if (negativeExponent) {
+			result = 1./result;
+		}
+		return result;
+	}
+	public static void main( String args[]) {
+		System.out.println("test powIterative(2,3) expect 8. get " + powIterative(2,3));
+		System.out.println("test powIterative(2,-3) expect 0.125 get " + powIterative(2,-3));
+		System.out.println("test powIterative(-2,3) expect -8. get " + powIterative(-2,3));
+		System.out.println("test powIterative(-2,0) expect 1. get " + powIterative(2,0));
+	}
+	
+
+}
