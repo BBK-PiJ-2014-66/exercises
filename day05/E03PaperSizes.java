59693b88fff2c7c25a4febae292f68d789ed5415
Oliver Smart <osmart@smartsci.uk>
2014-10-28 09:13:55 +0000

    deal with just A0 so far

--- day05/E03PaperSizes.java
+++ day05/E03PaperSizes.java
@@ -9,11 +9,11 @@
  * 
  *	A Din-A0 sheet of paper is 841mm × 1189mm (its surface is one
  *	square meter). Successive measurements of paper are defined
- *	recursively as “half” or “double” the preceding size. Therefore,
+ *	recursively as "half" or "double" the preceding size. Therefore,
  *	a Din-A1 sheet of paper is half of Din-A0, or 594mm × 841mm;
  *	while a Din-A00 is double of Din-A0, or 1189mm× 1682mm.  Create
  *	a method that takes a String parameter representing a size (e.g.
- *	”A4”, “A00000”) and prints on screen the size of the
+ *	"A4", "A00000") and prints on screen the size of the
  *	corresponding sheet of paper. For simplicity, you can ignore
  *	rounding errors when calculating smaller sizes.
  *	
@@ -26,3 +26,31 @@
  * Initial thoughts
  *
  */
+class PaperDinA {
+	private int mmSizeSmall;
+	private int mmSizeLarge;
+	/*    intSize 	PaperSize
+		-2	   A000
+		-1	   A00
+		 0   	   A0 
+		 1	   A1
+		 4 	   A4 */
+	private int intSize; 
+	private static int mmSizeSmallA0 = 841;
+	private static int mmSizeLargeA0 = 1189;
+	public PaperDinA() { // default constructor to A0
+		mmSizeSmall = mmSizeSmallA0;
+		mmSizeLarge = mmSizeLargeA0;
+		intSize = 0;
+	}
+	@Override
+    	public String toString() {
+        	return "" + mmSizeSmall + " mm x " + mmSizeLarge + " mm";
+   	}
+}
+class E03PaperSizes{
+	public static void main(String[] args) {
+		PaperDinA A0 = new PaperDinA();
+		System.out.println("test A0 = " + A0);
+	}
+}
