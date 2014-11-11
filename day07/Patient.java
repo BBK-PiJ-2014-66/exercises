3dbcf657e5b1743d33e02133264aa19baa656ecb
Oliver Smart <osmart@smartsci.uk>
2014-11-11 17:42:33 +0000

    #3 Singly-linked lists Patient.java from notes

---
+++ day07/Patient.java
@@ -0,0 +1,22 @@
+/* 
+ * Birbeck MSc Computer Science PiJ Exercsies 
+ * author Oliver S. Smart
+ * date   from 11 Nov 2014
+ *  
+ * Day 7 Exercise 3 Singly-linked lists
+ *
+ * Patient class from the notes
+ */
+public class Patient {
+	private String name;
+	private int age;
+	private String illness;
+	private Patient nextPatient;
+
+	public Patient( String name, int age, String illness) {
+		this.name = name;
+		this.age = age;
+		this.illness = illness;
+		this.nextPatient = null;
+	}
+}
