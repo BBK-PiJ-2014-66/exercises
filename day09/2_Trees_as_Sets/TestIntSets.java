ad274f2be6877143eb40d30279af95c11b7f739b
Oliver Smart <osmart01@dcs.bbk.ac.uk>
2014-11-24 23:35:02 +0000

    extend test set to around 26 values.
    $ java TestIntSets
    Run tests on TreeIntSet:
    add 7 to the set
    add 9 to the set
    add 7 to the set again!
    print the set contents: 7, 9
    verbose test whether set contains 9:
    	contains checks 9 against 7
    	contains checks 9 against 9
    	Result set does contain 9
    extend set to: 7, 3, 1, 9, 8, 49, 13, 29, 22, 21, 19, 14, 18, 23, 27, 26, 45, 35, 34, 33, 37, 41, 39, 43, 44, 50
    verbose test whether set contains 19:
    	contains checks 19 against 7
    	contains checks 19 against 9
    	contains checks 19 against 49
    	contains checks 19 against 13
    	contains checks 19 against 29
    	contains checks 19 against 22
    	contains checks 19 against 21
    	contains checks 19 against 19
    	Result set does contain 19
    verbose test whether set contains 28:
    	contains checks 28 against 7
    	contains checks 28 against 9
    	contains checks 28 against 49
    	contains checks 28 against 13
    	contains checks 28 against 29
    	contains checks 28 against 22
    	contains checks 28 against 23
    	contains checks 28 against 27
    	Result set does not contain 28
    
    So Tree implementation can check for a value with 7 or 8 comparisons.

--- day09/2_Trees_as_Sets/TestIntSets.java
+++ day09/2_Trees_as_Sets/TestIntSets.java
@@ -23,8 +23,18 @@ public class TestIntSets{
 		System.out.println("print the set contents: " + testIntSet);
 		System.out.println("verbose test whether set contains 9: ");
 		System.out.println("\tResult set does "+ (testIntSet.containsVerbose(9) ? "" : "not ") + "contain 9");
+		int someInts[] = {  3, 13, 45, 13, 34, 23, 37, 34, 27, 43, 22, 8, 50, 1, 39, 18 };
+		for (int i= 0; i < someInts.length; i++) {
+			int anInt = someInts[i];
+			testIntSet.add(anInt);
+			testIntSet.add(50-(17*anInt)%50);
+		}
+		System.out.println("extend set to: " + testIntSet);
 		System.out.println("verbose test whether set contains 19: ");
 		System.out.println("\tResult set does "+ (testIntSet.containsVerbose(19) ? "" : "not ") + "contain 19");
+		System.out.println("verbose test whether set contains 28: ");
+		System.out.println("\tResult set does "+ (testIntSet.containsVerbose(28) ? "" : "not ") + "contain 28");
+
 
 	
 	}
