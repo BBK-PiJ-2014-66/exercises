6f60ac23d93f381aacd8164db25b7c29bfbca844
Oliver Smart <osmart01@dcs.bbk.ac.uk>
2014-11-24 14:45:27 +0000

    Test "complete" version of toString.   Output-> -> Output agrees with that in exercise sheet Figure 1.
    $ java TestIntegerTreeNode
    Create a tree like in exercise sheet Figure 1 with 6, 9, 5, 3, 11, 12, 8
    Point (a) have started with 6 as head. toString gives: [6 L[] R[]]
    Point (b) after 6, 9. toString gives: [6 L[] R[9 L[] R[]]]
    Point (c) after 6, 9, 5, 3 toString gives: [6 L[5 L[3 L[] R[]] R[]] R[9 L[] R[]]]
    Point (d) after 6, 9, 5, 3, 11, 12, 8 toString gives: [6 L[5 L[3 L[] R[]] R[]] R[9 L[8 L[] R[]] R[11 L[] R[12 L[] R[]]]]]
    check whether tree contains 11 (it does) gives: true
    check whether tree contains 10 (it does not) gives: false
    The maximum value in the tree is 12, getMax gives: 12
    The maximum value in the tree is 3, getMin gives: 3

--- day09/1_Integer_Binary_Tree/TestIntegerTreeNode.java
+++ day09/1_Integer_Binary_Tree/TestIntegerTreeNode.java
@@ -12,14 +12,18 @@ public class TestIntegerTreeNode{
 		launch();
 	}
 	public static void launch() {
+		System.out.println("Create a tree like in exercise sheet Figure 1 with 6, 9, 5, 3, 11, 12, 8");
 		IntegerList testIntTree = new IntegerTreeNode(6);
+		System.out.println("Point (a) have started with 6 as head. toString gives: " + testIntTree);
 		testIntTree.add(9);
+		System.out.println("Point (b) after 6, 9. toString gives: " + testIntTree);
 		testIntTree.add(5);
 		testIntTree.add(3);
+		System.out.println("Point (c) after 6, 9, 5, 3 toString gives: " + testIntTree);
 		testIntTree.add(11);
 		testIntTree.add(12);
 		testIntTree.add(8);
-		System.out.println("Created tree like in Notes Figure 2 with 6. 9, 5, 3, 11, 12, 8");
+		System.out.println("Point (d) after 6, 9, 5, 3, 11, 12, 8 toString gives: " + testIntTree);
 		System.out.println("check whether tree contains 11 (it does) gives: " + testIntTree.contains(11));
 		System.out.println("check whether tree contains 10 (it does not) gives: " + testIntTree.contains(10));
 		System.out.println("The maximum value in the tree is 12, getMax gives: " + testIntTree.getMax());
