0efc6727fd6fbcf7b9f9fb25190c5d9dd508bf7e
Oliver Smart <osmart01@dcs.bbk.ac.uk>
2014-11-25 19:52:59 +0000

    Use modified IntegerTreeNode... Test output -> class rather than a new one. Makes no sense to produce a seperate set version as most of it is the same. Instead simply have an addSet method for set versus list.
    -> Test output remains the same:
    
    $ java TestIntSets
    Run tests on TreeIntSet:
    add 7 to the set
    add 9 to the set
    add 7 to the set again!
    print the set contents: 7, 9
    verbose test whether set contains 9:
    	contains checks 9 against 7
    	contains checks 9 against 9
    	Result set does contain 9
    extend set to: 7, 3, 1, 9, 8, 49, 13, 29, 22, 21, 19, 14, 18, 23, 27, 26, 45, 35, 34, 33, 37, 41, 39, 43, 44, 50
    verbose test whether set contains 19:
    	contains checks 19 against 7
    	contains checks 19 against 9
    	contains checks 19 against 49
    	contains checks 19 against 13
    	contains checks 19 against 29
    	contains checks 19 against 22
    	contains checks 19 against 21
    	contains checks 19 against 19
    	Result set does contain 19
    verbose test whether set contains 28:
    	contains checks 28 against 7
    	contains checks 28 against 9
    	contains checks 28 against 49
    	contains checks 28 against 13
    	contains checks 28 against 29
    	contains checks 28 against 22
    	contains checks 28 against 23
    	contains checks 28 against 27
    	Result set does not contain 28
    
    Re-Run the same tests on ListIntSet:
    add 7 to the set
    add 9 to the set
    add 7 to the set again!
    print the set contents: 7, 9
    verbose test whether set contains 9:
    	contains checks 9 against 7
    	contains checks 9 against 9
    	Result set does contain 9
    extend set to: 7, 9, 3, 49, 13, 29, 45, 35, 34, 22, 23, 37, 21, 27, 41, 43, 19, 26, 8, 14, 50, 1, 33, 39, 18, 44
    verbose test whether set contains 19:
    	contains checks 19 against 7
    	contains checks 19 against 9
    	contains checks 19 against 3
    	contains checks 19 against 49
    	contains checks 19 against 13
    	contains checks 19 against 29
    	contains checks 19 against 45
    	contains checks 19 against 35
    	contains checks 19 against 34
    	contains checks 19 against 22
    	contains checks 19 against 23
    	contains checks 19 against 37
    	contains checks 19 against 21
    	contains checks 19 against 27
    	contains checks 19 against 41
    	contains checks 19 against 43
    	contains checks 19 against 19
    	Result set does contain 19
    verbose test whether set contains 28:
    	contains checks 28 against 7
    	contains checks 28 against 9
    	contains checks 28 against 3
    	contains checks 28 against 49
    	contains checks 28 against 13
    	contains checks 28 against 29
    	contains checks 28 against 45
    	contains checks 28 against 35
    	contains checks 28 against 34
    	contains checks 28 against 22
    	contains checks 28 against 23
    	contains checks 28 against 37
    	contains checks 28 against 21
    	contains checks 28 against 27
    	contains checks 28 against 41
    	contains checks 28 against 43
    	contains checks 28 against 19
    	contains checks 28 against 26
    	contains checks 28 against 8
    	contains checks 28 against 14
    	contains checks 28 against 50
    	contains checks 28 against 1
    	contains checks 28 against 33
    	contains checks 28 against 39
    	contains checks 28 against 18
    	contains checks 28 against 44
    	Result set does not contain 28

---
+++ day09/2_Trees_as_Sets/IntegerTreeNode.java
@@ -0,0 +1,166 @@
+/* 
+ * Birbeck MSc Computer Science PiJ Exercises 
+ * author: Oliver S. Smart
+ * date:   from 24 Nov 2014
+ *  
+ * Day 9 Exercise 5.1 Integer Binary Tree
+ *
+ * IntegerTreeNode class as given in notes, completed
+ * Then extended for exercise 2 and exercise 3
+ */
+public class IntegerTreeNode {
+	int value;
+	IntegerTreeNode left;
+	IntegerTreeNode right;
+
+	IntegerTreeNode( int newNumber) { // creates the head or a leaf 
+		value = newNumber;
+		left = null;
+		right = null;
+	}
+
+	public void add( int newNumber) { // adds a new number to the tree as given in notes
+		if (newNumber > this.value) {
+			if (right == null) {
+				right = new IntegerTreeNode( newNumber);
+			} else {
+				right.add(newNumber);
+			}
+		} else {
+			if (left == null) {
+				left = new IntegerTreeNode( newNumber);
+			} else {
+				left.add(newNumber);
+			}
+		}
+	}
+
+	// version of add for sets - no duplicates allowed
+	public void addSet( int newNumber) { // adds a new number to the set 
+		if (newNumber == value) { // do nothing simply return
+		} else if (newNumber > value) {
+                        if (right == null) {
+                                right = new IntegerTreeNode( newNumber);
+                        } else {
+                                right.addSet(newNumber);
+                        }
+                } else {
+                        if (left == null) {
+                                left = new IntegerTreeNode( newNumber);
+                        } else {
+                                left.addSet(newNumber);
+                        }
+                }
+        }
+
+
+
+	public boolean contains( int n) {
+		return containsVerbose( n, false);
+	}
+
+
+	public boolean containsVerbose( int inNum, boolean verbose) {
+	 	if (verbose) {
+			System.out.println("\tcontains checks " + inNum + " against " + value);
+		}
+		if (inNum > value) {
+			if (right == null) {
+				return false;
+			} else {
+				return right.containsVerbose( inNum, verbose);
+			}
+		} else if (value == inNum) {
+			return true;
+		} else  {
+			if (left == null) {
+				return false;
+			} else {
+				return left.containsVerbose( inNum, verbose);
+			}
+		}
+	}
+
+
+	public int getMax() { // returns maximum value in the tree 
+		if (right == null) {
+			return value;
+		} else {
+			return right.getMax();
+		}
+	}
+	
+	public int getMin() { // returns minimum value in the tree 
+		if (left == null) {
+			return value;
+		} else {
+			return left.getMin();
+		}
+	}
+
+        @Override
+        public String toString() { // "simplified" print in exercise
+		String retStr= "[" + value;
+		if (left!=null) {
+			retStr += " " + left; // toString will be invoked
+		}
+		if (right!=null) {
+			retStr += " " + right; 
+		}
+		retStr += "]";
+		return retStr;
+	}
+
+        public String toStringComplete() { // "complete" print in exercise
+		String retStr= "[" + value;
+		if (left==null) {
+			retStr += " L[]";
+		} else	 {
+			retStr += " L" + left; // toString will be invoked
+		}
+		if (right==null) {
+			retStr += " R[]";
+		} else	 {
+			retStr += " R" + right; 
+		}
+		retStr += "]";
+		return retStr;
+	}
+
+        public String toStringCommaSeparated() { 
+                String retStr= "" + value;
+                if (left!=null) {
+                        retStr += ", " + left.toStringCommaSeparated(); 
+                }
+                if (right!=null) {
+                        retStr += ", " + right.toStringCommaSeparated();
+                }
+                return retStr;
+        }
+
+	public String information() {
+		return "The tree contains " + this.numberValues() + 
+			" values. It has a depth of " + this.depth();
+	}
+
+	private int depth() { // returns the depth that is the number of levels below (1.3)
+		if (left==null && right==null) {
+			return 0; // leaf has depth zero
+		} else if (left==null) {
+			return 1 + right.depth();
+		} else if (right==null) {
+			return 1 + left.depth();
+		} else {
+			return 1 + Math.max(right.depth(),left.depth());
+		}
+	}
+
+	private int numberValues() {
+		int retNumber = 1; // this node
+		if (left!=null)
+			retNumber += left.numberValues();
+		if (right!=null)
+			retNumber += right.numberValues();
+		return retNumber;
+	}
+}
