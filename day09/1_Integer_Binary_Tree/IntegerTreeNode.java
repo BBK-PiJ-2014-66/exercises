91cc0ef4f58cf6a4e7ae93bcab2c7f55c39f32e5
Oliver Smart <osmart01@dcs.bbk.ac.uk>
2014-11-27 21:54:19 +0000

    code up remove() method  ... Output-> ... works. Use hint from worksheet.
    -> Output of test shows that remove works
    
    	$ java TestIntegerTreeNode
    	Create a tree like in exercise sheet Figure 1 with 6, 9, 5, 3, 11, 12, 8
    	Note toString now simplified
    	Point (a) have started with 6 as head. toString gives: [6]
    	   The tree contains 1 values. It has a depth of 0, sorted values:6
    	Point (b) after 6, 9. toString gives: [6 [9]]
    	   The tree contains 2 values. It has a depth of 1, sorted values:6, 9
    	Point (c) after 6, 9, 5, 3 toString gives: [6 [5 [3]] [9]]
    	   The tree contains 4 values. It has a depth of 2, sorted values:3, 5, 6, 9
    	Point (d) after 6, 9, 5, 3, 11, 12, 8 toString gives: [6 [5 [3]] [9 [8] [11 [12]]]]
    	   The tree contains 7 values. It has a depth of 3, sorted values:3, 5, 6, 8, 9, 11, 12
    	check whether tree contains 11 (it does) gives: true
    	check whether tree contains 10 (it does not) gives: false
    	The maximum value in the tree is 12, getMax gives: 12
    	The maximum value in the tree is 3, getMin gives: 3
    
    	Now remove 8:
    	debug remove 8 take out leaf to the left
    	after removal:[6 [5 [3]] [9 [11 [12]]]]   The tree contains 6 values. It has a depth of 3, sorted values:3, 5, 6, 9, 11, 12
    
    	Now remove 6:
    	debug remove 6 got a non-leaf match
    	debug remove 5 got a non-leaf match
    	debug remove 3 take out leaf to the left
    	after removal:[5 [3] [9 [11 [12]]]]   The tree contains 5 values. It has a depth of 3, sorted values:3, 5, 9, 11, 12
    
    	Now remove 11:
    	debug remove 11 got a non-leaf match
    	debug remove 12 take out leaf to the right
    	after removal:[5 [3] [9 [12]]]   The tree contains 4 values. It has a depth of 2, sorted values:3, 5, 9, 12
    
    	Now remove 3:
    	debug remove 3 take out leaf to the left
    	after removal:[5 [9 [12]]]   The tree contains 3 values. It has a depth of 2, sorted values:5, 9, 12
    
    	Now remove 12:
    	debug remove 12 take out leaf to the right
    	after removal:[5 [9]]   The tree contains 2 values. It has a depth of 1, sorted values:5, 9

--- day09/1_Integer_Binary_Tree/IntegerTreeNode.java
+++ day09/1_Integer_Binary_Tree/IntegerTreeNode.java
@@ -53,8 +53,6 @@ public class IntegerTreeNode {
                 }
         }
 
-
-
 	public boolean contains( int n) {
 		return containsVerbose( n, false);
 	}
@@ -153,8 +151,70 @@ public class IntegerTreeNode {
 	}
 
 	public String information() {
-		return "The tree contains " + this.numberValues() + 
-			" values. It has a depth of " + this.depth();
+		return "   The tree contains " + this.numberValues() + 
+			" values. It has a depth of " + this.depth() + 
+			", sorted values:" + this.toStringInOrderCommaSeparated();
+	}
+
+
+	public void remove( int inNum) { 
+ 		/* removes a value from the list 
+		 * does nothing if called with a value that is not in the list
+		 *
+		 * only actually removes a node that is a leaf. 
+                 * Otherwise replaces leaves the node intact but
+		 * replaces its value with the highest value on the left
+                 * or if this is null the lowest value on the right.
+                 * then invokes remove to remove that value from the appropriate branch. 
+		 *
+		 * note that removal of the last number in the list has to be done elsewhere
+		 */
+
+		// first remove a matching leaf below to the left 
+		if (left != null) {
+			if ( (left.value == inNum) && (left.left == null) && (left.right == null)) {
+				System.out.println("debug remove " + inNum + " take out leaf to the left");
+				left = null; // take out the pointer
+				return;
+			}
+		}
+
+		// do same on the right
+		if (right != null) {
+			if ( (right.value == inNum) && (right.left == null) && (right.right == null)) {
+				System.out.println("debug remove " + inNum + " take out leaf to the right ");
+				right = null; // take out the pointer
+				return;
+			}
+		}
+		
+		
+		if (inNum > value) { // node to remove should be on the right
+			if (right != null) {
+				right.remove( inNum);
+			}
+		} else if (value == inNum) { /* need to remove value from this node
+					      * follow sheet "Hint: removing leafs is trivial; to remove nodes, 
+                                              * you can replace the removed node with its highest element on
+					      * its left or the lowest on its right."
+                                              * do this by taking the finding the value from the highest 
+                                              * element on the left or lowest on the right. Remove this value
+                                              * from the tree then replace the currents node value with it.
+					      */
+			System.out.println("debug remove " + inNum + " got a non-leaf match ");
+			if (left != null) { // replace value with largest value on the left
+				int replace = left.getMax();
+				this.remove(replace); 
+				value = replace; 
+			} else if (right != null) { // or smallest on the left
+				int replace = right.getMin();
+				this.remove(replace); 
+				value = replace; 
+			}
+		} else if (left != null) {
+			left.remove(inNum);
+		}
+		
 	}
 
 	private int depth() { // returns the depth that is the number of levels below (1.3)
