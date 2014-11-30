7a89b494d808221d16a9a07514c6f665bfc844ef
Oliver Smart <osmart01@dcs.bbk.ac.uk>
2014-11-30 22:13:46 +0000

    interface as instructed

---
+++ day10/src/Phone.java
@@ -0,0 +1,12 @@
+/** 
+ * A phone makes calls
+ * @author Oliver Smart <osmart01@dcs.bbk.ac.uk>
+ */
+public interface Phone {
+	/**
+	 * Just print on the screen "Calling <number> ...".
+	 *
+	 * @param number phone number to call
+	 */
+	void call( String number);
+}
