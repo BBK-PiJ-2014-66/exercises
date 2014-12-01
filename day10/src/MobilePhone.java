f470d11ebf9cea1b719366ba00f4e4f477d15416
Oliver Smart <osmart01@dcs.bbk.ac.uk>
2014-12-01 09:54:40 +0000

    MobilePhone... Test output-> extends OldPhone has last number store, plays games and has an alarm
    -> Test program PhoneLauncher tests all these features
    
    $ java PhoneLauncher
    Test call a couple of numbers:
    Calling <020 7631 6000>
    Calling <+1 202 456 1111>
    
    This phone stores the last 10 numbers that have been called
    test print of the last number store:
    The last 2 numbers rung:
    	+1 202 456 1111
    	020 7631 6000
    
    Test call 10 more numbers:
    Calling <020 7631 6800>
    Calling <020 7631 6801>
    Calling <020 7631 6802>
    Calling <020 7631 6803>
    Calling <020 7631 6804>
    Calling <020 7631 6805>
    Calling <020 7631 6806>
    Calling <020 7631 6807>
    Calling <020 7631 6808>
    Calling <020 7631 6809>
    Print of the last number store again:
    The last 10 numbers rung:
    	020 7631 6809
    	020 7631 6808
    	020 7631 6807
    	020 7631 6806
    	020 7631 6805
    	020 7631 6804
    	020 7631 6803
    	020 7631 6802
    	020 7631 6801
    	020 7631 6800
    
    This phone has an Alarm:
    Ringing alarm 'annoying.mp3'
    
    This phone can play games:
    Play game 'Poker.class'

---
+++ day10/src/MobilePhone.java
@@ -0,0 +1,76 @@
+/** 
+ * Mobile Phone
+ * @author Oliver Smart <osmart01@dcs.bbk.ac.uk>
+ */
+public class MobilePhone extends OldPhone {
+	// although we are told to store ten numbers do not hard code this
+	public static int MAX_NUMBERS_STORED=10;
+	private int numInLastNumberStore;
+	private String[] lastNumberStore;
+
+	/**
+	 * Default constructor: initializes lastNumbersStore
+	 */
+	MobilePhone() {
+		numInLastNumberStore = 0;
+		lastNumberStore = new String[MAX_NUMBERS_STORED];
+	}
+
+
+	/**
+	 * print on the screen "Calling <number> ..." and store the number
+	 *
+	 * @param number phone number to call
+	 */
+	public void call( String number) {
+		super.call(number);
+		store(number);
+	}
+	/**
+	 * rings a particular Alarm.
+	 *
+	 * @param alarm to ring (well print) 
+	 */
+	public void ringAlarm( String alarm) {
+		System.out.println("Ringing alarm '" + alarm + "'");
+	}
+
+	/**
+	 * rings a particular Alarm.
+	 *
+	 * @param alarm to ring (well print) 
+	 */
+	public void playGame( String game) {
+		System.out.println("Play game '" + game + "'");
+	}
+
+	/**
+	 * prints the last MAX_NUMBERS_STORED numbers rung
+	 *
+	 */
+	public void printLastNumbers() {
+		System.out.println("The last " + numInLastNumberStore + " numbers rung: ");
+		for (int nc =numInLastNumberStore-1; nc >=0 ; nc--) {
+			System.out.println("\t" + lastNumberStore[nc]);
+		}
+	}
+
+	/**
+	 *
+	 * stores the phone number 
+         * @param number phone number to store 
+         */ 
+	private void store( String number) {
+		if (numInLastNumberStore < MAX_NUMBERS_STORED) {
+			lastNumberStore[numInLastNumberStore] = number;
+			numInLastNumberStore++;
+		} else { // already filled store shuffle numbers down overwriting oldest
+			for (int nc = 0; nc< MAX_NUMBERS_STORED-1; nc++) {
+				lastNumberStore[nc] = lastNumberStore[nc+1];
+			}
+			lastNumberStore[numInLastNumberStore-1] = number;
+		}
+	}
+
+
+}
--- day10/src/PhoneLauncher.java
+++ day10/src/PhoneLauncher.java
@@ -8,12 +8,28 @@ public class PhoneLauncher {
 		launcher.launch();
 	}
 	public void launch() {
-		Phone myPhone = new OldPhone();
-		// call a couple of numbers
+		MobilePhone myPhone = new MobilePhone();
+
+		System.out.println("Test call a couple of numbers:");
 		myPhone.call("020 7631 6000"); // Birkbeck
-		myPhone.call("+1 202 456 1111"); // The White House	
-		// call 10 more
+		myPhone.call("+1 202 456 1111"); // The White House
+
+		System.out.println("\nThis phone stores the last " + myPhone.MAX_NUMBERS_STORED + " numbers that have been called");
+		System.out.println("test print of the last number store:");
+		myPhone.printLastNumbers();
+
+		System.out.println("\nTest call 10 more numbers:");
 		for (int tc= 0; tc<10; tc++)
 			myPhone.call("020 7631 680"+tc); 
+		System.out.println("Print of the last number store again:");
+		myPhone.printLastNumbers();
+
+		System.out.println("\nThis phone has an Alarm:");
+		myPhone.ringAlarm("annoying.mp3");
+
+		System.out.println("\nThis phone can play games:");
+		myPhone.playGame("Poker.class");
+
+
 	}
 }
