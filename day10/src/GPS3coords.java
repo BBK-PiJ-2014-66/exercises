a77956bf66e5f602df72b85230cdd1455ea542a8
Oliver Smart <osmart01@dcs.bbk.ac.uk>
2014-12-01 11:25:13 +0000

    Class with public fields to represent GPS coordinates

---
+++ day10/src/GPS3coords.java
@@ -0,0 +1,22 @@
+/** 
+ * GPS coordinates including altitude
+ * @author Oliver Smart <osmart01@dcs.bbk.ac.uk>
+ */
+public class GPS3coords{
+	/**
+	 * in decimal degrees (see http://www.gps-coordinates.net/) 
+	 */
+	public double latitude; 
+
+	/**
+	 * in decimal degrees 
+	 */
+	public double longitude; 
+
+	/**
+	 * in metres
+	 */
+	public double altitude; 
+
+}
+
