be06e0ecde036425f448ca2e31c0d2f2d9ce1d21
Oliver Smart <osmart01@dcs.bbk.ac.uk>
2014-12-08 13:18:23 +0000

    I think this is an improvement. Note that by... ... altering string version to use back converted number result will be different for inputs "1", "0002" as it will be "2" not "0002"

--- day11/src/comparator/Comparator.java
+++ day11/src/comparator/Comparator.java
@@ -1,13 +1,14 @@
 package comparator;
 
+/**
+ * PiJ exercises day 11
+ * 
+ * Exercise 1 Don't Repeat Yourself
+ * 
+ * @author Oliver Smart <osmart01@dcs.bbk.ac.uk>
+ * 
+ */
 public class Comparator {
-	public int getMax(int n, int m) {
-		if (n > m) {
-			return n;
-		} else {
-			return m;
-		}
-	}
 
 	public double getMax(double d1, double d2) {
 		if (d1 > d2) {
@@ -17,13 +18,30 @@ public class Comparator {
 		}
 	}
 
+	public int getMax(int n, int m) {
+		return (int) getMax((double) n, (double) m);
+	}
+
+	/**
+	 * 
+	 * method extended to check validity
+	 * 
+	 * @param number1
+	 *            that should contain a valid integer number
+	 * @param number2
+	 *            that should contain a valid integer number
+	 * @return string with the large integer number or null if either string is
+	 *         invalid
+	 */
 	public String getMax(String number1, String number2) {
-		int n1 = Integer.parseInt(number1);
-		int n2 = Integer.parseInt(number2);
-		if (n1 > n2) {
-			return number1;
-		} else {
-			return number2;
+		try {
+			int n1 = Integer.parseInt(number1);
+			int n2 = Integer.parseInt(number2);
+			int nMax = getMax(n1, n2);
+			return Integer.toString(nMax);
+		} catch (NumberFormatException e) {
+			// one of them not an integer!
+			return null;
 		}
 	}
 }
