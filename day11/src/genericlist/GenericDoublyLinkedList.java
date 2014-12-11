d02e3627bbfdf86e713bc0a89e9a852e5eebe03e
Oliver Smart <osmart01@dcs.bbk.ac.uk>
2014-12-11 17:45:28 +0000

    Doubly linked list that stores generic type initial "stub" version that implements interface MyGenericList<T>. Does not actually do anything but will be used for writing JUnit test.

---
+++ day11/src/genericlist/GenericDoublyLinkedList.java
@@ -0,0 +1,52 @@
+package genericlist;
+
+
+/**
+ * Doubly linked list that stores generic type
+ * 
+ * For PiJ day 11 exercise 3. 
+ * 
+ * Code adapted from LinkedList implementation in coursework3.
+ * 
+ * @author Oliver Smart <osmart01@dcs.bbk.ac.uk>
+ * 
+ */
+public class GenericDoublyLinkedList<T> implements MyGenericList<T> {
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public T get(int index) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public T remove(int index) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void add(int index, T item) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void add(T item) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
