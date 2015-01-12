33012483555fca2ae4869b7125f120384b8d2e01
Oliver Smart <osmart01@dcs.bbk.ac.uk>
2015-01-12 19:26:29 +0000

    initial stub Implementation. Test runs but fails.

---
+++ day13/src/BookImpl.java
@@ -0,0 +1,22 @@
+
+public class BookImpl implements Book {
+
+	private String author;
+	private String title;
+	
+	BookImpl( String author, String title) {
+			
+	}
+	
+	
+	@Override
+	public String getAuthor() {
+		return null;
+	}
+
+	@Override
+	public String getTitle() {
+		return null;
+	}
+
+}
