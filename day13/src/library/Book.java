1d19ed3900a750f1a55f6d8c86da6fa00f9e810d
Oliver Smart <osmart01@dcs.bbk.ac.uk>
2015-01-12 20:04:58 +0000

    move Book* from default package to "library" package

---
+++ day13/src/library/Book.java
@@ -0,0 +1,29 @@
+package library;
+/**
+ * PiJ day 13 Test Driven Development 
+ * 
+ * 1.1 "Create a class for books. Books have authors and titles.
+ * The class should implement getters for both author name and title, but these
+ * must be set at construction time and never be modified afterwards"
+ * 
+ * So implementations of this class should have one constructor only 
+ *  Book(author,title);
+ * 
+ * @author Oliver Smart <osmart01@dcs.bbk.ac.uk>
+ * @since Jan 2015
+ */
+public interface Book {
+		
+	/**
+	 *  getter for the author Name
+	 * @return the author's Name
+	 */
+	String getAuthor();
+	
+	/**
+	 *  getter for the book Title
+	 * @return the book title
+	 */
+	String getTitle();
+	
+}
