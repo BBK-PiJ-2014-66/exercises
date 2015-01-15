b526157e1fd5d92ae1592ee57ad863daa0aede86
Oliver Smart <osmart01@dcs.bbk.ac.uk>
2015-01-15 12:20:25 +0000

    1.4 update interace to include methods getMaxBooksPerUser and setMaxBooksPerUser as per instructions

--- day13/src/library/Library.java
+++ day13/src/library/Library.java
@@ -1,12 +1,19 @@
 package library;
+
 /**
  * PiJ day 13 Test Driven Development
  * 
- * 1.3/1.4  The Interface for the Library (needed for 1.4) 
+ * 1.3/1.4 The Interface for the Library (needed for 1.3, implemented in 1.4)
+ * 
+ * 1.4: "Libraries have a name, set at construction time. They also have a
+ * “maximum number of books borrowed by the same person” policy (e.g. max three
+ * books per user), which can be updated at any time. Of course, they also have
+ * a method to get the maximum number of books to be borrowed at any time (e.g.
+ * getMaxBooksPerUser())."
  * 
  * @author Oliver Smart <osmart01@dcs.bbk.ac.uk>
  * @since Jan 2015
- *
+ * 
  */
 public interface Library {
 
@@ -26,4 +33,16 @@ public interface Library {
 	 * @return the libraryID
 	 */
 	int getID(String usersName);
+	
+	/**
+	 * getter for the maximum number of books that a user can borrow at a time 
+	 * @return the maximum number of books allowed for a user
+	 */
+	int getMaxBooksPerUser();
+	
+	/**
+	 * setter for the maximum number of books that a user can borrow at a time 
+	 * @param maxBooksPerUser the new maximum number of books allowed for a user
+	 */
+	void setMaxBooksPerUser( int maxBooksPerUser);
 }
