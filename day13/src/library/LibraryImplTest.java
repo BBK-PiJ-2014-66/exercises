ea6e5f044ac3a650c65e35fb1ee01bfe3712845d
Oliver Smart <osmart01@dcs.bbk.ac.uk>
2015-01-16 10:51:58 +0000

    Add tests that default value of maxBooksPerUser is 3 as expected. Plus that after setting a value we can get it back.
    Get test fails with the MockLibrary implementation:
    
    testExpectDefaultMaxBooksPerUserEquals3(library.LibraryImplTest)
    java.lang.AssertionError: Default maxBooksPerUser should be three. expected:<3> but was:<0>
    	at org.junit.Assert.fail(Assert.java:91)
    	at org.junit.Assert.failNotEquals(Assert.java:645)
    	at org.junit.Assert.assertEquals(Assert.java:126)
    	at org.junit.Assert.assertEquals(Assert.java:470)
    	at library.LibraryImplTest.testExpectDefaultMaxBooksPerUserEquals3(LibraryImplTest.java:43)
            .....
    
    testSetMaxBooksPerUser(library.LibraryImplTest)
    java.lang.AssertionError: After .setMaxBooksPerUser(5) then .getMaxBooksPerUser() must return same value expected:<5> but was:<0>
    	at org.junit.Assert.fail(Assert.java:91)
    	at org.junit.Assert.failNotEquals(Assert.java:645)
    	at org.junit.Assert.assertEquals(Assert.java:126)
    	at org.junit.Assert.assertEquals(Assert.java:470)
    	at library.LibraryImplTest.testSetMaxBooksPerUser(LibraryImplTest.java:53)
            ..................
    
    testGetBackConstructorName(library.LibraryImplTest)
    org.junit.ComparisonFailure: get back initial library name expected:<[Test Library N]ame> but was:<[Mock Library n]ame>
    	at org.junit.Assert.assertEquals(Assert.java:123)
    	at library.LibraryImplTest.testGetBackConstructorName(LibraryImplTest.java:33)
            .....

--- day13/src/library/LibraryImplTest.java
+++ day13/src/library/LibraryImplTest.java
@@ -17,17 +17,42 @@ public class LibraryImplTest {
 	Library testLibrary;
 
 	/**
-	 * Initial version use MockLibrary to develop against rather LibraryImpl
+	 * initialize testLibrary with a specific implementation in initial version
+	 * use MockLibrary to develop against rather LibraryImpl
 	 */
 	@Before
 	public void initialize() {
 		testLibrary = new MockLibrary(TESTNAME);
 	}
 
+	/**
+	 * test that we get back the library name set in .initialize() method.
+	 */
 	@Test
 	public void testGetBackConstructorName() {
-		assertEquals("get back initial library name", TESTNAME, testLibrary.getLibrary());
+		assertEquals("get back initial library name", TESTNAME,
+				testLibrary.getLibrary());
 	}
 
+	/**
+	 * The interface comment says default number of books should be 3 so test
+	 * this
+	 */
+	@Test
+	public void testExpectDefaultMaxBooksPerUserEquals3() {
+		assertEquals("Default maxBooksPerUser should be three.", 3,
+				testLibrary.getMaxBooksPerUser());
+	}
+
+	/**
+	 * Use setMaxBooksPerUser and check that the number is recorded.
+	 */
+	@Test
+	public void testSetMaxBooksPerUser() {
+		testLibrary.setMaxBooksPerUser(5);
+		assertEquals(
+				"After .setMaxBooksPerUser(5) then .getMaxBooksPerUser() must return same value",
+				5, testLibrary.getMaxBooksPerUser());
+	}
 
 }
