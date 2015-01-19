8b17e3d729219b8dc079e23e52f471716c737237
Oliver Smart <osmart01@dcs.bbk.ac.uk>
2015-01-19 20:31:41 +0000

    Initial Stub implementation of Hailstone and a test that fails with a really informative message thanks to hamcrest:
    java.lang.AssertionError: Hailstone for input 3
    Expected: is <[10, 5, 16, 8, 4, 2, 1, 4, 2, 1]>
         got: null
    
    	at org.junit.Assert.assertThat(Assert.java:778)
    	at hailstone.HailstoneTest.testInput3(HailstoneTest.java:35)

---
+++ day14/src/hailstone/Hailstone.java
@@ -0,0 +1,34 @@
+package hailstone;
+
+import java.util.List;
+
+/**
+ * PiJ day 14 Recusion II
+ * 
+ * 3 Hailstone numbers
+ * 
+ * task:
+ * 
+ * The sequence hailstone numbers is defined as follows:
+ * 
+ * If the number n is even, the next number is n/2
+ * 
+ * If the number is odd, the next number is 3n + 1
+ * 
+ * It is thought that this sequence always converges to 1 (this is the Collatz
+ * conjecture, not demonstrated yet). Write a method that returns a list of
+ * integers (hint: you can use List<Integer> and ArrayList<Integer>) with the
+ * sequence of hailstone numbers that starts at some given natural number
+ * provided by the user.
+ * 
+ * When list hits 1 terminate
+ * 
+ * @author Oliver Smart <osmart01@dcs.bbk.ac.uk>
+ * @since Jan 2015
+ */
+public class Hailstone {
+	public static List <Integer> calculate( Integer iStart) {
+		return null;
+	}
+
+}
---
+++ day14/src/hailstone/HailstoneTest.java
@@ -0,0 +1,38 @@
+package hailstone;
+
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.CoreMatchers.*;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Test;
+/**
+ * PiJ day 14 Recusion II
+ * 
+ * 3 Hailstone numbers
+ * 
+ * JUnit tests
+ *
+ * @author Oliver Smart <osmart01@dcs.bbk.ac.uk>
+ * @since Jan 2015
+ */
+public class HailstoneTest {
+
+	@Test
+	public void testInput3() {
+		/*
+		 * https://www.math.hmc.edu/funfacts/ffiles/10008.5.shtml
+		 * gives hailstone for 3 as 10, 5, 16, 8, 4, 2, 1, 4, 2, 1
+		 */
+		List<Integer> actual = Hailstone.calculate(3);
+		List<Integer> expected = Arrays.asList(10, 5, 16, 8, 4, 2, 1, 4, 2, 1);
+		/*
+		 * great way of comparing Lists from
+		 * http://stackoverflow.com/questions/3236880/assert-list-in-junit
+		 * hamcrest rules!
+		 */
+		assertThat("Hailstone for input 3", actual, is(expected));
+	}
+
+}
