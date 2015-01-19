1055b7d42903b78560a6a1a2412347b962761a18
Oliver Smart <osmart01@dcs.bbk.ac.uk>
2015-01-19 14:36:47 +0000

    Start on Recursive version. using TDD
    Tests all fail with stub. Get trace:
    RecursiveTest
    e02anagrams.RecursiveTest
    testSingleLetterA(e02anagrams.RecursiveTest)
    input string 'A': arrays first differed at element [0]; expected:<[A]> but was:<[not yet implemented]>
    
    testAB(e02anagrams.RecursiveTest)
    java.lang.AssertionError: input string 'AB': array lengths differed, expected.length=2 actual.length=1
    
    testABC(e02anagrams.RecursiveTest)
    java.lang.AssertionError: input string 'ABC': array lengths differed, expected.length=6 actual.length=1
    Z

---
+++ day14/src/e02anagrams/Recursive.java
@@ -0,0 +1,42 @@
+package e02anagrams;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * PiJ day 14 Recusion II
+ * 
+ * 2
+ * 
+ * task: Anagrams
+ * 
+ * An anagram is a word created by recombination of the letters in another
+ * words. Some anagrams make sense ("silent", "listen") and some do not
+ * (“nietsl”). Some people only accepts “real” anagrams, but in this exercise we
+ * will accept them all, even if they do not exist as real words. Write a class
+ * with a static method that takes a String as a parameter and returns a list
+ * (hint: you can use List<String> and ArrayList<String>) of strings with all
+ * the anagrams that can be made with it.
+ * 
+ * 
+ * @author Oliver Smart <osmart01@dcs.bbk.ac.uk>
+ * @since Jan 2015
+ */
+public class Recursive {
+	/**
+	 * 
+	 * @param string
+	 *            the input string
+	 * @return a sorted list of the all non-real anagrams
+	 */
+	public static List<String> allNonRealAnagrams(String string) {
+		//catch null input
+		if (string == null) {
+			return null;
+		}
+		List<String> retList = new ArrayList<String>();
+		
+		retList.add("not yet implemented");
+		return retList;
+	}
+}
---
+++ day14/src/e02anagrams/RecursiveTest.java
@@ -0,0 +1,75 @@
+package e02anagrams;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.util.List;
+
+import org.junit.Test;
+
+/**
+ * PiJ day 14 Recusion II
+ * 
+ * 2 Anagrams
+ * 
+ * Tests for recursive version
+ * 
+ * @author Oliver Smart <osmart01@dcs.bbk.ac.uk>
+ * @since Jan 2015
+ */
+public class RecursiveTest {
+
+	/**
+	 * Wrapper for function
+	 */
+	public List<String> anagramWrapper(String string) {
+		return Recursive.allNonRealAnagrams(string);
+	}
+
+	/**
+	 * test a single letter input "A"
+	 */
+	@Test
+	public void testSingleLetterA() {
+		String input = "A";
+		String[] expect = { input };
+		List<String> getList = anagramWrapper(input);
+		assertNotNull("got null list for input string '"+ input+"'", getList);
+		// convert to list using method from
+		// http://stackoverflow.com/questions/4042434/convert-arraylist-containing-strings-to-an-array-of-strings-in-java
+		String[] getArray = getList.toArray(new String[getList.size()]);
+		assertArrayEquals("input string '"+ input+"'", expect, getArray);
+	}
+
+	/**
+	 * test "AB"
+	 */
+	@Test
+	public void testAB() {
+		String input = "AB";
+		String[] expect = { input, "BA" };
+		List<String> getList = anagramWrapper(input);
+		assertNotNull("got null list for input string '"+ input+"'", getList);
+		// convert to list using method from
+		// http://stackoverflow.com/questions/4042434/convert-arraylist-containing-strings-to-an-array-of-strings-in-java
+		String[] getArray = getList.toArray(new String[getList.size()]);
+		assertArrayEquals("input string '"+ input+"'", expect, getArray);
+	}
+	
+	/**
+	 * test "ABC"
+	 */
+	@Test
+	public void testABC() {
+		String input = "ABC";
+		String[] expect = { input, "ACB", "BAC", "BCA", "CAB", "CBA" };
+		List<String> getList = anagramWrapper(input);
+		assertNotNull("got null list for input string '"+ input+"'", getList);
+		// convert to list using method from
+		// http://stackoverflow.com/questions/4042434/convert-arraylist-containing-strings-to-an-array-of-strings-in-java
+		String[] getArray = getList.toArray(new String[getList.size()]);
+		assertArrayEquals("input string '"+ input+"'", expect, getArray);
+	}
+	
+
+}
