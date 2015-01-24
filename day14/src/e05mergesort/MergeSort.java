d04ec0ef7fab8a5fc1c332e0f0c578dbbc233f18
Oliver Smart <osmart01@dcs.bbk.ac.uk>
2015-01-24 10:33:50 +0000

    first idea - make generic

---
+++ day14/src/e05mergesort/MergeSort.java
@@ -0,0 +1,32 @@
+package e05mergesort;
+
+import java.util.List;
+
+/**
+ * PiJ day 14 Recusion II
+ * 
+ * 5 Merge Sort
+ * 
+ * task:
+ * 
+ * Mergesort is a popular sorting algorithm that employs a divide-and-conquer
+ * strategy. You can implement a Merge- sort for lists by following the
+ * following steps: Subproblem: If the list contains 0 or 1 element, it is
+ * sorted and you can return it. If not, then divide the list into two lists of
+ * the same length (± 1). Then sort the lists (i.e. apply mergesort to each
+ * sublist). Integration: When both sublists are returned sorted, check the
+ * first element of both sublists; remove the one that comes first (e.g. the
+ * lowest integer of the two) and add it to the result list. Repeat this process
+ * until all elements in both sublists have been added to the result list.
+ * Return the result list.
+ * 
+ * @author Oliver Smart <osmart01@dcs.bbk.ac.uk>
+ * @since 23 Jan 2015
+ */
+public class MergeSort<O> {
+
+	public List<O> mergeSort(List<O> inList) {
+		return null;
+	}
+
+}
