6a9127e10ec855f61f1d8e15abb1655ea067c5c9
Oliver Smart <osmart01@dcs.bbk.ac.uk>
2015-01-30 10:41:13 +0000

    JUnit test of isPrime. Passes Other tests fail because of stub methods

--- day15/src/e05primedivisorlist/PrimeDivisorListImplTest.java
+++ day15/src/e05primedivisorlist/PrimeDivisorListImplTest.java
@@ -65,7 +65,7 @@ public class PrimeDivisorListImplTest {
 				"Adding 7,3,2,3 and removing 7 produces correct .toString()",
 				actual, is(expected));
 	}
-	
+
 	@Test
 	public void testBadRemove() {
 		testPDL.addPrime(7);
@@ -76,8 +76,34 @@ public class PrimeDivisorListImplTest {
 			testPDL.remove(13);
 			fail(".remove(13) from a list that does not contain 13 failed to produce an IllegalArgumentException");
 		} catch (IllegalArgumentException ex) {
-			assertThat("IllegalArgument exception .remove(13) from list without 13",
+			assertThat(
+					"IllegalArgument exception .remove(13) from list without 13",
 					ex.getMessage(), is("list does not contain 13"));
+		}
+
+	}
+
+	@Test
+	public void testIsPrime() {
+		// from http://www.miniwebtool.com/list-of-prime-numbers/?to=100
+		int primesTo100[] = { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41,
+				43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97 };
+		for (int ic = 0; ic < primesTo100.length; ic++) {
+			int num = primesTo100[ic];
+			assertThat("isPrime(" + num + ") returns true",
+					PrimeDivisorListImpl.isPrime(num), is(true));
+		}
+
+		// from http://www.mathatube.com/files/Composite_Numbers.pdf
+		int compositeTo100[] = { 4, 6, 8, 9, 10, 12, 14, 15, 16, 18, 20, 21,
+				22, 24, 25, 26, 27, 28, 30, 32, 33, 34, 35, 36, 38, 39, 40, 42,
+				44, 45, 46, 48, 49, 50, 51, 52, 54, 55, 56, 57, 58, 60, 62, 63,
+				64, 65, 66, 68, 69, 70, 72, 74, 75, 76, 77, 78, 80, 81, 82, 84,
+				85, 86, 87, 88, 90, 91, 92, 93, 94, 95, 96, 98, 99, 100 };
+		for (int ic = 0; ic < compositeTo100.length; ic++) {
+			int num = compositeTo100[ic];
+			assertThat("isPrime(" + num + ") returns false",
+					PrimeDivisorListImpl.isPrime(num), is(false));
 		}
 
 	}
