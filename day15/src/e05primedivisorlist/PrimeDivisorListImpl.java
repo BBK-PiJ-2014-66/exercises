cf2f8ed8590828bd927d649cfab7ec0ce7479f91
Oliver Smart <osmart01@dcs.bbk.ac.uk>
2015-01-30 10:16:10 +0000

    Stub initial implementation. Fails JUnit test with trace:
    PrimeDivisorListImplTest
    e05primedivisorlist.PrimeDivisorListImplTest
    testAddNonPrimeProducesException(e05primedivisorlist.PrimeDivisorListImplTest)
    java.lang.AssertionError: Adding 4 as a prime failed to produce an IllegalArgumentException
    
    testSheetExample(e05primedivisorlist.PrimeDivisorListImplTest)
    java.lang.AssertionError: Adding 7,3,2,3 produces correct .toString()
    Expected: is "[ 2 * 3^2 *7 = 126 ]"
         got: "debug stub method PrimeDivisorListImpl.toString() needs to be written!"
    
    testOKRemove(e05primedivisorlist.PrimeDivisorListImplTest)
    java.lang.AssertionError: Adding 7,3,2,3 and removing 7 produces correct .toString()
    Expected: is "[ 2 * 3^2 = 18 ]"
         got: "debug stub method PrimeDivisorListImpl.toString() needs to be written!"
    
    testBadRemove(e05primedivisorlist.PrimeDivisorListImplTest)
    java.lang.AssertionError: .remove(13) from a list that does not contain 13 failed to produce an IllegalArgumentException
    
    testAddValidPrime(e05primedivisorlist.PrimeDivisorListImplTest)
    java.lang.AssertionError: Adding a single prime 3 produces correct .toString()
    Expected: is "[ 3 = 3 ]"
         got: "debug stub method PrimeDivisorListImpl.toString() needs to be written!"

---
+++ day15/src/e05primedivisorlist/PrimeDivisorListImpl.java
@@ -0,0 +1,35 @@
+package e05primedivisorlist;
+
+/**
+ * PiJ day 15 Exception handling
+ * 
+ * Exercise 5 Prime divisors
+ * 
+ * Implementation of interface PrimeDivisorList 
+ * 
+ * @author Oliver Smart <osmart01@dcs.bbk.ac.uk>
+ * @since 29 Jan 2015
+ */
+
+
+
+public class PrimeDivisorListImpl implements PrimeDivisorList {
+
+	// no constructor yet 
+	
+	@Override
+	public void addPrime(Integer aPrime) throws IllegalArgumentException {
+		System.out.println("debug stub method PrimeDivisorListImpl.addPrime() needs to be written!");
+	}
+
+	@Override
+	public void remove(Integer aNumber) throws IllegalArgumentException {
+		System.out.println("debug stub method PrimeDivisorListImpl.remove() needs to be written!");
+	}
+	
+	@Override
+	public String toString() {
+		return "debug stub method PrimeDivisorListImpl.toString() needs to be written!";
+	}
+
+}
