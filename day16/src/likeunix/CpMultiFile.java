9716d8a3111184b9c8802deb4c0df9a51ab7758d
Oliver Smart <osmart01@dcs.bbk.ac.uk>
2015-01-31 20:41:23 +0000

    split of individual copy to a function cp

--- day16/src/likeunix/CpMultiFile.java
+++ day16/src/likeunix/CpMultiFile.java
@@ -12,27 +12,23 @@ import java.util.Scanner;
  * 
  * PiJ day 16 I/O
  * 
- * Exercise 4 a) cp
+ * Exercise 4 b) cp multifile
  * 
- * Write a program that takes two names from the user at the command line. If a
- * file with the first name exists, the program must copy it into a file with
- * the second name. If the first file does not exist, the program must say so.
- * If the second file does exists, the program must ask the user whether to
- * overwrite it or not, and act accordingly.
  * 
- * So it is like cp -i
+ * Modify the program so that it takes many file names at the command line. When
+ * this happens, the last name must be a directory (otherwise, your program
+ * should complain). If it is a directory, your program has to copy all files
+ * (i.e. the other arguments) into that directory.
  * 
- * Assume we are mean to read the File line by line and write it a new one In
- * practice it would be much more sensible to use Files.copy() method!
  * 
  * @author Oliver Smart <osmart01@dcs.bbk.ac.uk>
  * @since 31 Jan 2015
  * 
  */
-public class CpMinusI {
+public class CpMultiFile {
 	public static void main(String args[]) {
 		// use launch to have a single System.exit(1) for ERROR
-		CpMinusI cpi = new CpMinusI();
+		CpMultiFile cpi = new CpMultiFile();
 		try {
 			cpi.launch(args);
 		} catch (RuntimeException ex) {
@@ -41,18 +37,33 @@ public class CpMinusI {
 	}
 
 	public void launch(String args[]) {
-		if (args.length != 2) {
+		if (args.length < 2) {
 			System.err
 					.println("You must specify the source and destination files");
 			System.err
 					.println("for example to copy 'source.txt' to 'dest.txt'");
 			System.err
-					.println("\tjava -classpath .... CpMinusI source.txt dest.txt\n");
+					.println("\tjava -classpath .... CpMultiFile source.txt dest.txt\n");
 			throw new RuntimeException(
 					"Error has not specified two command line arguments");
 		}
 
-		String sourceName = args[0];
+		if (args.length == 2) {
+			cp(args[0], args[1]);
+		}
+	}
+
+	/**
+	 * copies a single file from source to dest
+	 * 
+	 * @param sourceName
+	 *            a filename must exist
+	 * @param destName
+	 *            a destination filename cannot be a directory
+	 * @throws RuntimeException
+	 *             on any error
+	 */
+	private void cp(String sourceName, String destName) {
 		File source = new File(sourceName);
 		if (!source.exists()) {
 			System.err.println("ERROR source file " + sourceName
@@ -60,7 +71,6 @@ public class CpMinusI {
 			throw new RuntimeException("source file does not exist");
 		}
 
-		String destName = args[1];
 		File dest = new File(destName);
 		if (dest.isDirectory()) {
 			System.err.println("ERROR destination file " + destName
@@ -125,5 +135,6 @@ public class CpMinusI {
 			}
 			out.close();
 		}
+
 	}
 }
