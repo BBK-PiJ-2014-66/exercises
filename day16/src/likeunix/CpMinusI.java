9eeabcdf130effdd115065f2b263706b589ae4a5
Oliver Smart <osmart01@dcs.bbk.ac.uk>
2015-01-31 19:09:08 +0000

    Written cp - needs testing

--- day16/src/likeunix/CpMinusI.java
+++ day16/src/likeunix/CpMinusI.java
@@ -1,7 +1,12 @@
 package likeunix;
 
+import java.io.BufferedReader;
 import java.io.File;
-
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
 
 /**
  * 
@@ -29,30 +34,96 @@ public class CpMinusI {
 		// use launch to have a single System.exit(1) for ERROR
 		CpMinusI cpi = new CpMinusI();
 		try {
-		cpi.launch( args);
+			cpi.launch(args);
 		} catch (RuntimeException ex) {
-			System.exit(1); // set error indication 
-		}		
+			System.exit(1); // set error indication
+		}
 	}
-	
-	public void launch( String args[] ) {
+
+	public void launch(String args[]) {
 		if (args.length != 2) {
 			System.err
 					.println("You must specify the source and destination files");
 			System.err
 					.println("for example to copy 'source.txt' to 'dest.txt'");
-			System.err.println("\tjava -classpath .... CpMinusI source.txt dest.txt\n");
-			throw new RuntimeException("Error has not specified two command line arguments");
+			System.err
+					.println("\tjava -classpath .... CpMinusI source.txt dest.txt\n");
+			throw new RuntimeException(
+					"Error has not specified two command line arguments");
 		}
-		
-		/*
+
 		String sourceName = args[0];
 		File source = new File(sourceName);
 		if (!source.exists()) {
-			System.out.println("ERROR source file " + sourceName + " does not exist");
-			
+			System.err.println("ERROR source file " + sourceName
+					+ " does not exist");
+			throw new RuntimeException("source file does not exist");
 		}
-		*/
-	}
 
+		String destName = args[1];
+		File dest = new File(destName);
+		if (dest.isDirectory()) {
+			System.err.println("ERROR destination file " + destName
+					+ " is a directory" + "Script cannot copy with this!");
+			throw new RuntimeException("destination is directory");
+		} else if (dest.exists()) {
+			// check interactively whether to clobber
+			// use same message as linux cp -i
+			System.out.print("cp: overwrite `" + dest + "'? ");
+			Scanner input = new Scanner(System.in);
+			String inLine = input.nextLine();
+			input.close();
+			// do not overwrite unless they have entered y
+			if (inLine == null || inLine.length() < 1
+					|| !inLine.substring(0, 1).equalsIgnoreCase("y"))
+				return;
+		}
+
+		// open source as 'in'
+		BufferedReader in = null;
+		try {
+			in = new BufferedReader(new FileReader(source));
+		} catch (FileNotFoundException ex) {
+			// FileNotFoundException happens if the named file does not
+			// exist, is a directory rather than a regular file, or for some
+			// other reason cannot be opened for reading
+			System.err.println("ERROR opening source file " + ex.getMessage());
+			throw (new RuntimeException(ex));
+		}
+		// open dest as 'out'
+		PrintWriter out = null;
+		try {
+			out = new PrintWriter(dest);
+		} catch (FileNotFoundException ex) {
+			// FileNotFoundException - If the given file object does not denote
+			// an existing, writable regular file and a new regular file of that
+			// name cannot be created, or if some other error occurs while
+			// opening or creating the file
+			System.err.println("ERROR opening destination file"
+					+ ex.getMessage());
+			throw (new RuntimeException(ex));
+		}
+
+		// have opened both input and output files
+		String line;
+		try {
+			while ((line = in.readLine()) != null) {
+				out.println(line);
+			}
+		} catch (IOException ex) {
+			System.err.println("ERROR IO Exception reading from input file "
+					+ ex.getMessage());
+			throw (new RuntimeException(ex));
+		} finally {
+			// close both input and output files
+			try {
+				in.close();
+			} catch (IOException ex) {
+				System.err.println("ERROR closing source file "
+						+ ex.getMessage());
+				throw (new RuntimeException(ex));
+			}
+			out.close();
+		}
+	}
 }
