b50989046c4f404aa32eee6b573126949d822f9c
Oliver Smart <osmart01@dcs.bbk.ac.uk>
2015-02-01 10:39:31 +0000

    refactor to throw errors rather than directly printing to .err. Improve usage type message.

--- day16/src/likeunix/CpBinary.java
+++ day16/src/likeunix/CpBinary.java
@@ -33,27 +33,26 @@ public class CpBinary {
 		try {
 			cpi.launch(args);
 		} catch (RuntimeException ex) {
+			System.err.println(ex); // print exception to STDERR
 			System.exit(1); // set error indication
 		}
 	}
 
 	public void launch(String args[]) {
 		if (args.length < 2) {
-			System.err
-					.println("You must specify the source and destination files");
-			System.err
-					.println("for example to copy 'source.txt' to 'dest.txt'");
-			System.err
-					.println("\tjava -classpath .... CpBinary source.txt dest.txt\n");
-			throw new RuntimeException(
-					"Error has not specified two command line arguments");
+			String msg = "You must specify the source and destination files\n"
+					+ "for example to copy 'source.txt' to 'dest.txt'\n\n"
+					+ "\t\tjava -classpath .... CpBinary source.txt dest.txt\n\n"
+					+ "alternatively you can specify one or more files to be copied to a directory:\n\n"
+					+ "\tjava -classpath .... CpBinary file1 file2 destinationDirectory\n";
+			throw new RuntimeException(msg);
 		}
 
 		// if the last file specified a directory
 		String lastFileName = args[args.length - 1];
 		File lastFile = new File(lastFileName);
 		if (lastFile.isDirectory()) {
-			for (int fc=0; fc < args.length - 1; fc ++) {
+			for (int fc = 0; fc < args.length - 1; fc++) {
 				// copy this file into the destination directory
 				// n.b. two slashes do not matter in linux
 				String dest = lastFileName + "/" + args[fc];
@@ -65,7 +64,6 @@ public class CpBinary {
 		} else {
 			// give the same error as linux cp does in this situation
 			String msg = "cp: target `" + lastFileName + "' is not a directory";
-			System.err.println(msg);
 			throw new RuntimeException(msg);
 		}
 	}
@@ -83,16 +81,14 @@ public class CpBinary {
 	private void cp(String sourceName, String destName) {
 		File source = new File(sourceName);
 		if (!source.exists()) {
-			System.err.println("ERROR source file " + sourceName
+			throw new RuntimeException("ERROR source file " + sourceName
 					+ " does not exist");
-			throw new RuntimeException("source file does not exist");
 		}
 
 		File dest = new File(destName);
 		if (dest.isDirectory()) {
-			System.err.println("ERROR destination file " + destName
-					+ " is a directory" + "Script cannot copy with this!");
-			throw new RuntimeException("destination is directory");
+			throw new RuntimeException("ERROR destination file " + destName
+					+ " is a directory Script cannot copy with this!");
 		} else if (dest.exists()) {
 			// check interactively whether to clobber
 			// use same message as linux cp -i
@@ -114,8 +110,8 @@ public class CpBinary {
 			// FileNotFoundException happens if the named file does not
 			// exist, is a directory rather than a regular file, or for some
 			// other reason cannot be opened for reading
-			System.err.println("ERROR opening source file " + ex.getMessage());
-			throw (new RuntimeException(ex));
+			throw (new RuntimeException("ERROR opening source file "
+					+ ex.getMessage()));
 		}
 		// open dest as 'out'
 		OutputStream out = null;
@@ -126,9 +122,8 @@ public class CpBinary {
 			// an existing, writable regular file and a new regular file of that
 			// name cannot be created, or if some other error occurs while
 			// opening or creating the file
-			System.err.println("ERROR opening destination file "
-					+ ex.getMessage());
-			throw (new RuntimeException(ex));
+			throw (new RuntimeException("ERROR opening destination file "
+					+ ex.getMessage()));
 		}
 
 		// have opened both input and output files
@@ -138,24 +133,22 @@ public class CpBinary {
 				out.write(data);
 			}
 		} catch (IOException ex) {
-			System.err.println("ERROR IO Exception reading from input file "
-					+ ex.getMessage());
-			throw (new RuntimeException(ex));
+			throw (new RuntimeException(
+					"ERROR IO Exception reading from input file "
+							+ ex.getMessage()));
 		} finally {
 			// close both input and output files
 			try {
 				in.close();
 			} catch (IOException ex) {
-				System.err.println("ERROR closing source file "
-						+ ex.getMessage());
-				throw (new RuntimeException(ex));
+				throw (new RuntimeException("ERROR closing source file "
+						+ ex.getMessage()));
 			}
 			try {
 				out.close();
 			} catch (IOException ex) {
-				System.err.println("ERROR closing destination file "
-						+ ex.getMessage());
-				throw (new RuntimeException(ex));
+				throw (new RuntimeException("ERROR closing destination file "
+						+ ex.getMessage()));
 			}
 		}
 
