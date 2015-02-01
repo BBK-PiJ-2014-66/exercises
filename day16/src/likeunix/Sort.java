d94915dfa497a288a4b0ea1a70fc23d951b43397
Oliver Smart <osmart01@dcs.bbk.ac.uk>
2015-02-01 11:12:11 +0000

    Start on Sort - following my preferred approach

---
+++ day16/src/likeunix/Sort.java
@@ -0,0 +1,42 @@
+package likeunix;
+
+/**
+ * 
+ * PiJ day 16 I/O
+ * 
+ * Exercise 6 sort (*)
+ * 
+ * task:
+ * 
+ * Write a program that takes a name from the user at the command line. If a
+ * file with that name exists, the program must show its contents on screen, but
+ * with the lines shown alphabetically. If the does not exist, the program must
+ * say so.
+ * 
+ * @author Oliver Smart <osmart01@dcs.bbk.ac.uk>
+ * @since 01 Feb 2015
+ * 
+ */
+public class Sort {
+	public static void main(String args[]) {
+		// use launch to have a single System.exit(1) for ERROR
+		Sort sorti = new Sort();
+		try {
+			sorti.launch(args);
+		} catch (RuntimeException ex) {
+			System.err.println(ex.getMessage()); // print exception to STDERR
+			System.exit(1); // set error indication
+		}
+	}
+
+	public void launch(String args[]) {
+		if (args.length != 1) {
+			String msg = "You must a single input file to sort.\n"
+					+ "For example to sort 'file.txt`:\n\n"
+					+ "\t\tjava -classpath .... Sort file.txt\n\n";
+			throw new RuntimeException(msg);
+		}
+
+	}
+
+}
