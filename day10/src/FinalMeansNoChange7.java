21fcf07f13c4081ff5028e21889ba27664872256
Oliver Smart <osmart01@dcs.bbk.ac.uk>
2015-02-02 10:27:46 +0000

    forgot to commit this some time ago

--- day10/src/FinalMeansNoChange7.java
+++ day10/src/FinalMeansNoChange7.java
@@ -6,13 +6,15 @@
  * see what happens.
  * 
  */
-public class FinalMeansNoChange7 extends String {
+public class FinalMeansNoChange7 
+//extends String 
+{
 	public void printEven() {
 		// do not know that the string in String is called! so difficult to write
 		// assume it to be string String string = "1234567890";
-		for (int sc = 1; sc < string.length() ; sc += 2) {
+/*		for (int sc = 1; sc < string.length() ; sc += 2) {
 			System.out.print(string.charAt(sc));
 		}
-		System.out.println();
+		System.out.println(); */
 	}
 }
