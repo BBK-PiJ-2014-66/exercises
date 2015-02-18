7d8cb44a6e251ecd9e88567dd1eac64dba11c214
Oliver Smart <osmart01@dcs.bbk.ac.uk>
2015-02-18 16:29:00 +0000

    3 example using default to add a new method to an interface without breaking existing code

---
+++ day19/interfaces/src/basics/IntStack.java
@@ -0,0 +1,21 @@
+package basics;
+
+/**
+ * 
+ * Example of how to a method to an existing interface so as not to break
+ * existing code
+ * 
+ * @author PiJ sheet "Interfaces revisited (Java 8)" page 4
+ *
+ */
+public interface IntStack {
+	void push(int item); // store an item
+
+	int pop(); // retrieve an item
+
+	// Because clear() has a default, it need not be implemented by an existing
+	// class that implements IntStack
+	default void clear() {
+		System.out.println("clear() not implemented.");
+	}
+}
