162bedf5e546e87a7f4334f899308c096999eca5
Oliver Smart <osmart01@dcs.bbk.ac.uk>
2015-02-19 12:35:00 +0000

    try of UnaryOperator rather than own functional interface

--- day19/lambdas/src/four/LambdaDemo.java
+++ day19/lambdas/src/four/LambdaDemo.java
@@ -6,6 +6,8 @@
  */
 package four;
 
+import java.util.function.UnaryOperator;
+
 public class LambdaDemo {
 
 	public static void main(String[] args) {
@@ -17,6 +19,17 @@ public class LambdaDemo {
 			return result;
 		};
 		System.out.println("The factorial of 3 is " + factorial.func(3));
-		System.out.println("The factorial of 3 is " + factorial.func(5));
+		System.out.println("The factorial of 5 is " + factorial.func(5));
+		
+		// OSS for fun try out UnaryOperator in place of DIY functional interface
+		UnaryOperator<Integer> factorialU = (n) -> {
+			int result = 1;
+			for (int ic=n; ic>1; ic--)  {
+				result *= ic;
+			}
+			return result;
+		};
+		System.out.println("factorialU of 5 is " + factorialU.apply(5));
 	}
 }
+
