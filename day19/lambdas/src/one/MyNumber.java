b8a37e67bea886fbda567b019eb02eaf5fd93b52
Oliver Smart <osmart01@dcs.bbk.ac.uk>
2015-02-19 10:16:26 +0000

    Start on lambda sheet

---
+++ day19/lambdas/src/one/MyNumber.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ * @author PiJ sheet "An Introduction to Lambda Expressions in Java 8: Part 1" 
+ * page 4
+ *
+ */
+package one;
+
+// a functional interface
+public interface MyNumber {
+	double getValue();
+}
