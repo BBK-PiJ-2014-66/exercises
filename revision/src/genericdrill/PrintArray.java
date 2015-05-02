5e2a5e6598fc7524af8d0869fce66f43c7f8e88a
Oliver Smart <osmart01@dcs.bbk.ac.uk>
2015-05-02 14:13:46 +0100

    Actually include output

--- revision/src/genericdrill/PrintArray.java
+++ revision/src/genericdrill/PrintArray.java
@@ -4,6 +4,11 @@ package genericdrill;
  * Keith Mannock "Generics Drill" in Revision Session 27th April 2015
  * 
  * 1st example a simple generic method and how type inference works.
+ * 
+ * Produces output: 
+ * 		1 2 3 4 5
+ *		1.1 1.2 1.3 1.4 1.5
+ * 		a b c
  *
  */
 public class PrintArray {
@@ -11,19 +16,19 @@ public class PrintArray {
 	public static void main(String[] args) {
 		Integer[] intArray = { 1, 2, 3, 4, 5 };
 		Double[] doubleArray = { 1.1, 1.2, 1.3, 1.4, 1.5 };
-		Character[] characterArray = { 'a', 'b', 'c'};
-		
+		Character[] characterArray = { 'a', 'b', 'c' };
+
 		// OSS note using "type inference"
 		printArray(intArray);
 		printArray(doubleArray);
 		printArray(characterArray);
 	}
-	
-	public static <T> void printArray( T[] someArray) {
+
+	public static <T> void printArray(T[] someArray) {
 		for (T ele : someArray) {
 			System.out.print(" " + ele);
 		}
 		System.out.println();
-		
+
 	}
 }
