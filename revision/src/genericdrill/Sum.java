5240ffd2c17a79fd5cddef2f0dbb397662aab668
Oliver Smart <osmart01@dcs.bbk.ac.uk>
2015-05-02 18:02:02 +0100

    Code up a recursive version of sum.

--- revision/src/genericdrill/Sum.java
+++ revision/src/genericdrill/Sum.java
@@ -1,27 +1,34 @@
 package genericdrill;
 
+import java.util.ArrayList;
 import java.util.Arrays;
-
+import java.util.List;
 
 /**
  * Keith Mannock "Generics Drill" in Revision Session 27th April 2015
  * 
- * 3rd example: A generic example of Sum - that is impossible to write?
- * got very confusing .....
+ * 3rd example: A generic example of Sum - that is impossible to write? got very
+ * confusing .....
  * 
  * So I did by overloading instead.
  * 
- * Produces output:
- * 		 sum of [1, 2, 3, 4, 5] is 15
- * 		 sum of [1.1, 1.2, 1.3, 1.4, 1.5] is 6.5
+ * 4th example - getting sum recursively. This was also confused in class - so I
+ * did a recursive method using an ArrayList and remove
  * 
+ * Produces output:
+ * 
+ * sum of [1, 2, 3, 4, 5] is 15
+ * 
+ * sum of [1.1, 1.2, 1.3, 1.4, 1.5] is 6.5
+ * 
+ * recursive sum of [1.1, 1.2, 1.3, 1.4, 1.5] is 6.5
  *
  */
 public class Sum {
 
 	public static void main(String[] args) {
 		Integer[] intArray = { 1, 2, 3, 4, 5 };
-		Integer intSum = sumIterative(intArray); 
+		Integer intSum = sumIterative(intArray);
 
 		System.out.println(" sum of " + Arrays.asList(intArray) + " is "
 				+ intSum);
@@ -30,55 +37,81 @@ public class Sum {
 		Double dSum = sumIterative(doubleArray);
 		System.out.println(" sum of " + Arrays.asList(doubleArray) + " is "
 				+ dSum);
+		dSum = sumRecursive(doubleArray);
+		System.out.println(" recursive sum of " + Arrays.asList(doubleArray)
+				+ " is " + dSum);
 
 	}
 
-//	public static <T extends Number> T sumIterative(T... arr) {
-//		System.out.println("sumIterative arr.getClass()= " + arr.getClass().getName());
-//		/*
-//		 * Went through in class but I got confused - it appears task is not wise.
-//		 */
-//		// following line does not compile cannot make a new instance of T
-//		T result = 0.0;
-//		for (T ele : arr ) {
-//			// The following line does not compile as + is not defined for "Number"
-//			result = result + ele;
-//		}
-//	
-//        /*
-//         *  further attempt at home - getting no where		
-//         */
-//		System.out.println("sumIterative arr.getClass()= " + arr.getClass().getName());
-//		String className = arr.getClass().getName();
-//		if (className.contains("Integer") ) {
-//			int result = 0;
-//			for (T ele : arr) {
-//				result = result + ele.intValue();
-//			}
-//			return (T) result;
-//		} else {
-//			
-//		}
-//		return null;
-//	}
-	
+	// public static <T extends Number> T sumIterative(T... arr) {
+	// System.out.println("sumIterative arr.getClass()= " +
+	// arr.getClass().getName());
+	// /*
+	// * Went through in class but I got confused - it appears task is not wise.
+	// */
+	// // following line does not compile cannot make a new instance of T
+	// T result = 0.0;
+	// for (T ele : arr ) {
+	// // The following line does not compile as + is not defined for "Number"
+	// result = result + ele;
+	// }
+	//
+	// /*
+	// * further attempt at home - getting no where
+	// */
+	// System.out.println("sumIterative arr.getClass()= " +
+	// arr.getClass().getName());
+	// String className = arr.getClass().getName();
+	// if (className.contains("Integer") ) {
+	// int result = 0;
+	// for (T ele : arr) {
+	// result = result + ele.intValue();
+	// }
+	// return (T) result;
+	// } else {
+	//
+	// }
+	// return null;
+	// }
+
 	// can easily do by simple overloading
-	public static double sumIterative( Double ... darr) {
+	public static double sumIterative(Double... darr) {
 		double result = 0.;
-		if (darr.length>0) {
-			for ( double dit : darr) {
+		if (darr.length > 0) {
+			for (double dit : darr) {
 				result += dit;
 			}
 		}
 		return result;
 	}
 
-	public static int sumIterative( Integer ... iarr) {
+	public static int sumIterative(Integer... iarr) {
 		int result = 0;
-		if (iarr.length>0) {
-			for ( int iit : iarr) {
+		if (iarr.length > 0) {
+			for (int iit : iarr) {
 				result += iit;
 			}
+		}
+		return result;
+	}
+
+	private static double sumRecursive(Double... darr) {
+		double result = 0.0;
+		if (darr.length > 1) {
+			ArrayList<Double> dlist = new ArrayList<Double>(Arrays.asList(darr));
+			result = sumRecursive(dlist);
+		}
+		return result;
+	}
+
+	private static Double sumRecursive(ArrayList<Double> dlist) {
+		double result = 0.0;
+		if (dlist.size() == 1) {
+			result = dlist.get(0);
+		} else {
+			double first = dlist.get(0);
+			dlist.remove(0);
+			result = first + sumRecursive(dlist);
 		}
 		return result;
 	}
